0562955dc44cb5fbeff3e400e695eddbedcb08b0
LucasFDutra <lucasfelipedutra@gamil.com>
2020-01-22 16:56:19 -0300

    button change text

---
+++ rn-first-app/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, View, Button,
+} from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title='change Text' onPress={() => setOutputText('the text change')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

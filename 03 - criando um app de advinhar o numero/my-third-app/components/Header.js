f2a3d1fef85b37f99b39edc8085d3d54fe88d760
LucasFDutra <lucasfelipedutra@gamil.com>
2020-02-10 20:52:16 -0300

    mudando o estilo de acordo com a plataforma

--- 03 - criando um app de advinhar o numero/my-third-app/components/Header.js
+++ 03 - criando um app de advinhar o numero/my-third-app/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, Platform,
+} from 'react-native';
 import TitleText from './TitleText';
 import Colors from '../constants/colors';
+import colors from '../constants/colors';
 
 const Header = (props) => (
   <View style={styles.header}>
@@ -14,12 +17,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
   },
   headerTitle: {
-    color: '#f5f5f5',
+    color: Platform.OS === 'ios' ? colors.primary : '#f5f5f5',
   },
 });
 

df3bd4f57e0cb470e952357886372f8dad67fc72
LucasFDutra <lucasfelipedutra@gamil.com>
2020-02-13 00:28:28 -0300

    iniciando estilização

--- 04 - criando um app de receitas/recipe_app/screens/CategoriesScreen.js
+++ 04 - criando um app de receitas/recipe_app/screens/CategoriesScreen.js
@@ -7,28 +7,38 @@ import {
   FlatList,
 } from 'react-native';
 
-const CategoriesScreen = (props) => {
-  console.log(props);
-  return (
-    <View style={styles.screen}>
-      <Text>The categories screen!</Text>
-      <Button
-        title='Go to Meals!'
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'CategoryMeals',
-          });
-        }}
-      />
-    </View>
-  );
-};
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => (
+  <View style={styles.gridItem}>
+    <Text>{ itemData.item.title }</Text>
+  </View>
+);
+
+const CategoriesScreen = (props) => (
+  <FlatList
+    data={CATEGORIES}
+    renderItem={renderGridItem}
+    numColumns={2}
+    keyExtractor={(item, index) => item.id}
+  />
+);
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    height: 150,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 20,
   },
 });
 

3a2c5e4582798df892234533421efe15220041be
LucasFDutra <lucasfelipedutra@gamil.com>
2020-02-17 21:34:12 -0300

    inicializando redux

--- 04 - criando um app de receitas/recipe_app/App.js
+++ 04 - criando um app de receitas/recipe_app/App.js
@@ -4,35 +4,26 @@ import {
   Text,
   View,
 } from 'react-native';
-// import * as Font from 'expo-font';
-// import { AppLoading } from 'expo';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meal';
 
 enableScreens();
 
-// const featchFonts = async () => {
-//   await Font.loadAsync({
-//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-//   });
-// };
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  // const [fontLoaded, setFontLoaded] = useState(false);
-
-  // if (!fontLoaded) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={featchFonts}
-  //       onFinish={() => setFontLoaded(true)}
-  //     />
-  //   );
-  // }
-
   return (
-    <MealsNavigator />
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
 
